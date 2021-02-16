19928ff6d3bda24a7e3cbda1807f08a390f3c5bf
sergei_petriankin <Sergei_petriankin@epam.com>
2021-02-16 17:21:15 +0300

    Initial commit

---
+++ src/main/java/io/github/petriankin/speedcam/speedcamadmin/SpeedcamAdminApplication.java
@@ -0,0 +1,13 @@
+package io.github.petriankin.speedcam.speedcamadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedcamAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedcamAdminApplication.class, args);
+    }
+
+}
